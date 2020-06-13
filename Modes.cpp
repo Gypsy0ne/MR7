0590a3b5646f2268568e7faf78e154478a81803a
martinezpl <mszlesz@gmail.com>
2020-06-13 20:14:52 +0200

    Flag system mode.

--- Modes.cpp
+++ Modes.cpp
@@ -10,6 +10,35 @@ void justRead(int readChannels[]){
     printSensors(readChannels);
 }
 
+void flags(int readChannels[], bool sensorFlags[], int cycles[]){
+    if(SENSORS_QTY >= 8)
+        readSensors(A0, A1, readChannels);
+    else
+        readSensors(A0, readChannels);
+
+    for(int i = 0; i < 8; i++){
+        if(isDry(readChannels[i], MOISTURE_THRESHOLD_PERCENTAGE[i]) == 1)
+            sensorFlags[i] = 1;
+    }
+
+    if(SENSORS_QTY >= 8)
+        for(int i = 8; i < 16; i++){
+            if(isDry(readChannels[i], MOISTURE_THRESHOLD_PERCENTAGE[i]) == 1)
+                sensorFlags[i] = 1;
+    }
+
+    for(int i = 0; i < PUMPS_QTY; i++){
+        if(sensorFlags[i] == 1){
+            setPump(i);
+            delay((unsigned long)PUMP_WORKTIME_SECONDS[i]*1000);
+            cycles[i]++;
+        }
+    }
+
+    for(int i = 0; i < SENSORS_QTY; i++)
+        sensorFlags[i] = 0;
+}
+
 
 /* void pair(int channel, int moistureThresholdPercentage, int pumpsWorktimeSeconds, int readChannels[], int cycles[]){  
     if(map(readChannels[channel], DRY_THRESHOLD, WET_THRESHOLD, 0, 100) < moistureThresholdPercentage){
--- Modes.h
+++ Modes.h
@@ -2,20 +2,9 @@
 #define Modes_h
 
 // Header for working modes of the MR7. 
-/* Pumps always activate one after another. No simultaneity possible using mux-transistor circuit. If needed, multiple pumps can be connected to a single mux channel (if DC source can provide enough current).
-If any pump is activated during the cycle, sensors need 20-60 seconds to stabilize their state. That's (probably) because the muxes are not seperated electrically. 
-When sensors are in various moisture zones, they need time to calibrate before program starts (40-60 seconds). This is not an issue when they are in similar moisture. (wtf?)*/
 
 void justRead();
 
-// void pair(int channel, int moistureThresholdPercentage, int pumpsWorktimeSeconds, int readChannels[], int cycles[]);
-/* Sensors and pumps work in pairs (Channel 0 sensor on mux1 - channel 0 pump on mux2 etc.)
-Sets a single pair on the same channel. */
-
-/*
-void allPairsEqual(int Seconds_Between_Checks, int moistureThresholdPercentage, int pumpsWorktimeSeconds);
-/* Sensors and pumps work in pairs (Channel 0 sensor on mux1 - channel 0 pump on mux2 etc.)
-All pumps have common moisture threshold, below which they are activated. 
-*/
+void flags(int readChannels[], bool sensorFlags[], int cycles[]);
 
 #endif
