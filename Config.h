9e832846dd35b45b921b2576eaaae1efd3fc2ed6
martinezpl <mszlesz@gmail.com>
2020-06-13 20:13:45 +0200

    thresholds & worktimes set in config

--- Config.h
+++ Config.h
@@ -5,6 +5,9 @@
 const int DRY_THRESHOLD = 610;
 const int WET_THRESHOLD = 255;
 const float INPUT_VOLTAGE = 5.0;
+//                                             0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15
+const int MOISTURE_THRESHOLD_PERCENTAGE[16] = {50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50};
+const int PUMP_WORKTIME_SECONDS[16] =         {5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5};
 
 //Arduino
 const int SR_LATCH = 2; // Shift Register Controls. vv
@@ -17,10 +20,10 @@ const int SR_DATA = 4;
 
 
 //Sensors
-const int SENSORS_QTY = 5; // Quantity of sensors.
+const int SENSORS_QTY = 16; // Quantity of sensors.
 const int SENSOR_CALIBRATING_TIME = 60;
 
 //Pumps
-const int PUMPS_QTY = 5; // Quantity of pumps.
+const int PUMPS_QTY = 16; // Quantity of pumps.
 
 #endif  
