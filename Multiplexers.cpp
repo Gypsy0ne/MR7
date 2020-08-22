d4925295844c64d0795d5e081185392aa5ef06b9
martinezpl <mszlesz@gmail.com>
2020-08-22 22:47:41 +0200

    Multiplexers

--- Multiplexers.cpp
+++ Multiplexers.cpp
@@ -1,136 +1,112 @@
 #include "Multiplexers.h"
 
-#include "Config.h"
 #include "ShiftRegister.h"
 #include "Arduino.h"
 
-void setSensorMuxChannel(int channel) { // The same channel is set for both sensor MUXes. One MUX is connected to, for example, A0 and the other A1. 
-    SR_resetStorage();
+// Hardware note: The same channel is set for both sensor MUXes. One MUX is connected to, for example, A0 and the other A1. 
+void SensorMuxDuo::setChannel(int channel) { 
+    ShiftRegister SR;
     switch(channel) { // Q0/a Q1/b Q2/c
         case 0: // 0 0 0 
-            SR_activateLatch();
             delay(1); // Give the mux some time to process the redirection.
             break;
         case 1: // 1 0 0
-            SR_addBit(1);
-            SR_activateLatch();
+            SR.latchBitSequence(1);
             delay(1);
             break;
         case 2: // 0 1 0
-            SR_addBitSequence(10);
-            SR_activateLatch();
+            SR.latchBitSequence(10);
             delay(1);
             break;
         case 3: // 1 1 0
-            SR_addBit(1, 2);
-            SR_activateLatch();
+            SR.latchBitSequence(11);
             delay(1);
             break;
         case 4: // 0 0 1
-            SR_addBitSequence(100);
-            SR_activateLatch();
+            SR.latchBitSequence(100);
             delay(1);
             break;
         case 5: // 1 0 1
-            SR_addBitSequence(101);
-            SR_activateLatch();
+            SR.latchBitSequence(101);
             delay(1);
             break;
         case 6: // 0 1 1
-            SR_addBitSequence(110);
-            SR_activateLatch();
+            SR.latchBitSequence(110);
             delay(1);
             break;
         case 7:  // 1 1 1
-            SR_addBit(1, 3);
-            SR_activateLatch();
+            SR.latchBitSequence(111);
             delay(1);
             break;
     }
 }
 
-
-void setPumpMuxChannel(int channel) { // Activates pump on a given channel.
-    SR_resetStorage(); 
+// Hardware note: Same channel is set for both muxes, last two digits determine which one gets high input.
+void PumpMuxDuo::setChannel(int channel) { 
+    ShiftRegister SR;
     switch(channel) { // Q3/a Q4/b Q5/c
         case 0: // 0 0 0 0 0 0  0 1
-            SR_addBitSequence(10000000);
-            SR_activateLatch();
+            SR.latchBitSequence(10000000);
             delay(1); // Give the mux some time to process the redirection.
             break;
         case 1: // 0 0 0 1 0 0  0 1
-            SR_addBitSequence(10001000);
-            SR_activateLatch();
+            SR.latchBitSequence(10001000);
             delay(1);
             break;
         case 2: // 0 0 0 0 1 0  0 1
-            SR_addBitSequence(10010000);
-            SR_activateLatch();
+            SR.latchBitSequence(10010000);
             delay(1);
             break;
         case 3: // 0 0 0 1 1 0  0 1
-            SR_addBitSequence(10011000);
-            SR_activateLatch();
+            SR.latchBitSequence(10011000);
             delay(1);
             break;
         case 4: // 0 0 0 0 0 1  0 1
-            SR_addBitSequence(10100000);
-            SR_activateLatch();
+            SR.latchBitSequence(10100000);
             delay(1);
             break;
         case 5: // 0 0 0 1 0 1  0 1
-            SR_addBitSequence(10101000);
-            SR_activateLatch();
+            SR.latchBitSequence(10101000);
             delay(1);
             break;
         case 6: // 0 0 0 0 1 1  0 1
-            SR_addBitSequence(10110000);
-            SR_activateLatch();
+            SR.latchBitSequence(10110000);
             delay(1);
             break;
         case 7: // 0 0 0 1 1 1  0 1
-            SR_addBitSequence(10111000);
-            SR_activateLatch();
+            SR.latchBitSequence(10111000);
             delay(1);
             break;
         case 8: // 0 0 0 0 0 0  1 0
-            SR_addBitSequence(1000000);
-            SR_activateLatch();
+            SR.latchBitSequence(1000000);
             delay(1); // Give the mux some time to process the redirection.
             break;
         case 9: // 0 0 0 1 0 0  1 0
-            SR_addBitSequence(1001000);
-            SR_activateLatch();
+            SR.latchBitSequence(1001000);
             delay(1);
             break;
         case 10: // 0 0 0 0 1 0  1 0
-            SR_addBitSequence(1010000);
-            SR_activateLatch();
+            SR.latchBitSequence(1010000);
             delay(1);
             break;
         case 11: // 0 0 0 1 1 0  1 0
-            SR_addBitSequence(1011000);
-            SR_activateLatch();
+            SR.latchBitSequence(1011000);
             delay(1);
             break;
         case 12: // 0 0 0 0 0 1  1 0
-            SR_addBitSequence(1100000);
-            SR_activateLatch();
+            SR.latchBitSequence(1100000);
             delay(1);
             break;
         case 13: // 0 0 0 1 0 1  1 0
-            SR_addBitSequence(1101000);
-            SR_activateLatch();
+            SR.latchBitSequence(1101000);
             delay(1);
             break;
         case 14: // 0 0 0 0 1 1  1 0
-            SR_addBitSequence(1110000);
-            SR_activateLatch();
+            SR.latchBitSequence(1110000);
             delay(1);
             break;
         case 15: // 0 0 0 1 1 1  1 0
-            SR_addBitSequence(1111000);
-            SR_activateLatch();
+            SR.latchBitSequence(1111000);
             delay(1);
             break;
     }
--- Multiplexers.h
+++ Multiplexers.h
@@ -3,9 +3,20 @@
 
 // MODEL: CD4051BM https://www.ti.com/lit/ds/symlink/cd4051b.pdf?&ts=1589798832366
 
-// Sets multiplexer control pins to a state combination unlocking a given channel.
-void setSensorMuxChannel(int channel); 
-void setPumpMuxChannel(int channel);
+class Multiplexer {
+    public:
+        virtual void setChannel(int channel)=0;
+};
+
+class PumpMuxDuo : Multiplexer {
+    public:
+        void setChannel(int channel);
+};
+
+class SensorMuxDuo : Multiplexer {
+    public:
+        void setChannel(int channel);
+};
 
 #endif
 
