7adf0a1f6258312ab2631962c4fb19809c1b4ac8
martinezpl <mszlesz@gmail.com>
2020-08-22 20:45:24 +0200

    SR_addBitSequence in work

--- Multiplexers.cpp
+++ Multiplexers.cpp
@@ -4,7 +4,7 @@
 #include "ShiftRegister.h"
 #include "Arduino.h"
 
-void setSensorMuxChannel(int channel) { // Both sensor MUXes are set to the same channel. One MUX is connected to, for example, A0 and the other A1. 
+void setSensorMuxChannel(int channel) { // The same channel is set for both sensor MUXes. One MUX is connected to, for example, A0 and the other A1. 
     SR_resetStorage();
     switch(channel) { // Q0/a Q1/b Q2/c
         case 0: // 0 0 0 
@@ -17,8 +17,7 @@ void setSensorMuxChannel(int channel) { // Both sensor MUXes are set to the same
             delay(1);
             break;
         case 2: // 0 1 0
-            SR_addBit(1);
-            SR_addBit(0);
+            SR_addBitSequence(10);
             SR_activateLatch();
             delay(1);
             break;
@@ -28,21 +27,17 @@ void setSensorMuxChannel(int channel) { // Both sensor MUXes are set to the same
             delay(1);
             break;
         case 4: // 0 0 1
-            SR_addBit(1);
-            SR_addBit(0, 2);
+            SR_addBitSequence(100);
             SR_activateLatch();
             delay(1);
             break;
         case 5: // 1 0 1
-            SR_addBit(1);
-            SR_addBit(0);
-            SR_addBit(1);
+            SR_addBitSequence(101);
             SR_activateLatch();
             delay(1);
             break;
         case 6: // 0 1 1
-            SR_addBit(1, 2);
-            SR_addBit(0);
+            SR_addBitSequence(110);
             SR_activateLatch();
             delay(1);
             break;
@@ -59,138 +54,82 @@ void setPumpMuxChannel(int channel) { // Activates pump on a given channel.
     SR_resetStorage(); 
     switch(channel) { // Q3/a Q4/b Q5/c
         case 0: // 0 0 0 0 0 0  0 1
-            SR_addBit(0);
-            SR_addBit(1);        
-            SR_addBit(0, 5);
+            SR_addBitSequence(10000000);
             SR_activateLatch();
             delay(1); // Give the mux some time to process the redirection.
             break;
         case 1: // 0 0 0 1 0 0  0 1
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(0, 2);
-            SR_addBit(1);
-            SR_addBit(0, 3);
+            SR_addBitSequence(10001000);
             SR_activateLatch();
             delay(1);
             break;
         case 2: // 0 0 0 0 1 0  0 1
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(0, 4);
+            SR_addBitSequence(10010000);
             SR_activateLatch();
             delay(1);
             break;
         case 3: // 0 0 0 1 1 0  0 1
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(0);
-            SR_addBit(1, 2);
-            SR_addBit(0, 3);
+            SR_addBitSequence(10011000);
             SR_activateLatch();
             delay(1);
             break;
         case 4: // 0 0 0 0 0 1  0 1
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(1);
-            SR_addBit(0, 5);
+            SR_addBitSequence(10100000);
             SR_activateLatch();
             delay(1);
             break;
         case 5: // 0 0 0 1 0 1  0 1
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(1);
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(1, 3);
+            SR_addBitSequence(10101000);
             SR_activateLatch();
             delay(1);
             break;
         case 6: // 0 0 0 0 1 1  0 1
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(1, 2);
-            SR_addBit(0, 4);
+            SR_addBitSequence(10110000);
             SR_activateLatch();
             delay(1);
             break;
         case 7: // 0 0 0 1 1 1  0 1
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(1, 3);
-            SR_addBit(0, 3);
+            SR_addBitSequence(10111000);
             SR_activateLatch();
             delay(1);
             break;
         case 8: // 0 0 0 0 0 0  1 0
-            SR_addBit(1);
-            SR_addBit(0);        
-            SR_addBit(0, 5);
+            SR_addBitSequence(1000000);
             SR_activateLatch();
             delay(1); // Give the mux some time to process the redirection.
             break;
         case 9: // 0 0 0 1 0 0  1 0
-            SR_addBit(1);
-            SR_addBit(0);
-            SR_addBit(0, 2);
-            SR_addBit(1);
-            SR_addBit(0, 3);
+            SR_addBitSequence(1001000);
             SR_activateLatch();
             delay(1);
             break;
         case 10: // 0 0 0 0 1 0  1 0
-            SR_addBit(1);
-            SR_addBit(0);
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(0, 4);
+            SR_addBitSequence(1010000);
             SR_activateLatch();
             delay(1);
             break;
         case 11: // 0 0 0 1 1 0  1 0
-            SR_addBit(1);
-            SR_addBit(0);
-            SR_addBit(0);
-            SR_addBit(1, 2);
-            SR_addBit(0, 3);
+            SR_addBitSequence(1011000);
             SR_activateLatch();
             delay(1);
             break;
         case 12: // 0 0 0 0 0 1  1 0
-            SR_addBit(1);
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(0, 5);
+            SR_addBitSequence(1100000);
             SR_activateLatch();
             delay(1);
             break;
         case 13: // 0 0 0 1 0 1  1 0
-            SR_addBit(1);
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(0);
-            SR_addBit(1);
-            SR_addBit(1, 3);
+            SR_addBitSequence(1101000);
             SR_activateLatch();
             delay(1);
             break;
         case 14: // 0 0 0 0 1 1  1 0
-            SR_addBit(1);
-            SR_addBit(0);
-            SR_addBit(1, 2);
-            SR_addBit(0, 4);
+            SR_addBitSequence(1110000);
             SR_activateLatch();
             delay(1);
             break;
         case 15: // 0 0 0 1 1 1  1 0
-            SR_addBit(1);
-            SR_addBit(0);
-            SR_addBit(1, 3);
-            SR_addBit(0, 3);
+            SR_addBitSequence(1111000);
             SR_activateLatch();
             delay(1);
             break;
