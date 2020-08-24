1f0e3a53e26924d9650c5365ffd52d9f80a4d262
martinezpl <mszlesz@gmail.com>
2020-08-24 13:12:07 +0200

    inline constructors

--- Multiplexers.h
+++ Multiplexers.h
@@ -4,17 +4,21 @@
 // MODEL: CD4051BM https://www.ti.com/lit/ds/symlink/cd4051b.pdf?&ts=1589798832366
 
 class Multiplexer {
-    public:
+    protected:
         virtual void setChannel(int channel)=0;
 };
 
 class PumpMuxDuo : Multiplexer {
     public:
+        inline PumpMuxDuo(int channel) { setChannel(channel) }
+    private:
         void setChannel(int channel);
 };
 
 class SensorMuxDuo : Multiplexer {
     public:
+        inline SensorMuxDuo(int channel) { setChannel(channel) }
+    private:
         void setChannel(int channel);
 };
 
