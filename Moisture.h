d829f9b3cee8b2c9f659fabc4f6e2ed213b22cc0
martinezpl <mszlesz@gmail.com>
2020-08-24 13:11:26 +0200

    obsolete

--- Moisture.h
+++
@@ -1,9 +0,0 @@
-#ifndef Moisture_h
-#define Moisture_h
-
-#include "Arduino.h"
-
-int readMoistureInPercent(int& sensorRead);
-float readMoistureInVolts(int& sensorRead);
-
-#endif
--- Temperature.h
+++
@@ -1,6 +0,0 @@
-#ifndef Temperature_h
-#define Temperature_h
-
-float callTempSensors();
-
-#endif
