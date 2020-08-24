0109bf87f0fe86fb3141566df723d28b8f6744a8
martinezpl <mszlesz@gmail.com>
2020-08-24 13:11:36 +0200

    Sensors

--- Sensors.cpp
+++ Sensors.cpp
@@ -1,81 +1,123 @@
 #include "Sensors.h"
 
-#include "Config.h"
-#include "Moisture.h"
 #include "Multiplexers.h"
-#include "Time.h"
 #include "Arduino.h"
 
-void getSensorReads(int analogPin1, int readChannels[]) {
-    for(int i = 0; i < 8; i++) {
-        setSensorMuxChannel(i);
-        readChannels[i] = analogRead(analogPin1);
-    }
+int MoistureSensor::readValue() {
+    SensorMuxDuo smd(_muxChannel);
+    return analogRead(_analogPin);
 }
 
-void getSensorReads(int analogPin1, int analogPin2, int readChannels[]) {
-    for(int i = 0; i < SENSORS_QTY; i++) {
-        setSensorMuxChannel(i);
-        if(i <= 8) {
-            readChannels[i] = analogRead(analogPin1);
-        }
-        else {
-            readChannels[i] = analogRead(analogPin2);
-        }
-    }
+int MoistureSensor::mapValue() {
+    _value = map(readValue(), _maxValue, _minValue, 0, 100); 
+    return _value;
 }
 
-void printSensorReads(int readChannels[], int cycles[]) {
-    for(int i = 0; i < SENSORS_QTY; i++) {
-        Serial.print((String)" " + i + "(" + cycles[i] + ")" + ": " + readMoistureInPercent(readChannels[i]) + "%");
-    }
-    Serial.print("\n");
-}
-
-void printSensorReads(int readChannels[]) {
-    for(int i = 0; i < SENSORS_QTY; i++) {
-        Serial.print((String)" " + i + ": " + readMoistureInPercent(readChannels[i]) + "%" + " (" + readChannels[i] + ")");
-    }
-    Serial.print("\n");
-}
-
-bool isDry(int& read, const int moistureThresholdPercentage) {
-    if(map(read, DRY_THRESHOLD, WET_THRESHOLD, 0, 100) < moistureThresholdPercentage) {
+bool MoistureSensor::isDry() {
+    Serial.println((String)"S" + _ID + ": " + _value + "% /n");
+    if (mapValue() < _thresholdPercentage) {
+        _flag = 1;
         return 1;
-    }
-    else {
+    } else {
         return 0;
     }
 }
 
-void collectFlags(int readChannels[], bool sensorFlags[]) {
-    for(int i = 0; i < SENSORS_QTY; i++) {
-        if(isDry(readChannels[i], MOISTURE_THRESHOLD_PERCENTAGE[i])) {
-            sensorFlags[i] = 1;
-        }
-    }
-}
+// ========================================================
 
-void clearFlags(bool sensorFlags[]) {
-    for(int i = 0; i < SENSORS_QTY; i++) {
-        sensorFlags[i] = 0;
+float TSensorsBus::callSensors() {
+    OneWire ds(_digitalPin);
+    // if 1, addr adopts the address of a found device
+    if ( !ds.search(addr)) { 
+        Serial.println("No more addresses.");
+        Serial.println();
+        ds.reset_search();
+        delay(250);
+        return 0;
     }
-}
+    
+    Serial.print("ROM =");
+    for( i = 0; i < 8; i++) {
+        Serial.write(' ');
+        Serial.print(addr[i], HEX);
+    }
 
-void calibrateSensors() {
-    unsigned long t = millis();
-    int seconder = SENSOR_CALIBRATING_TIME;
-    Serial.println((String)"Calibrating sensors... " + "[" + (seconder) + "s left]");                    
-    do {
-        if(countSeconds(10, t)) {
-            Serial.println((String)"Calibrating sensors... " + "[" + (seconder) + "s left]");
-            seconder -= 10;
-            t = millis();
-            for(int i = 0; i < SENSORS_QTY; i++) {
-                setSensorMuxChannel(i);
-                analogRead(A0);
-                analogRead(A1); // After pumps interrupt sensor reads, further read values are going crazy. Reading the sensors seems to be necessary for stabilization.
-            }
+   // CRC (byte) - Cyclic Redundancy Check, it's a code generated from memory data that is recalculated by the master bus to ensure error free transmission.
+    if (OneWire::crc8(addr, 7) != addr[7]) { 
+        Serial.println("CRC is not valid!");
+        return 0;
+    }
+    Serial.println();
+    
+    // the first ROM byte indicates which chip
+    switch (addr[0]) {
+        case 0x10:
+            Serial.println("  Chip = DS18S20");  // or old DS1820
+            type_s = 1;
+            break;
+        case 0x28:
+            Serial.println("  Chip = DS18B20");
+            type_s = 0;
+            break;
+        case 0x22:
+            Serial.println("  Chip = DS1822");
+            type_s = 0;
+            break;
+        default:
+            Serial.println("Device is not a DS18x20 family device.");
+            return 0;
+    } 
+
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44, 0);        // start conversion, for parasite power ds.write(0x44, 1);
+    
+    delay(1000);     // maybe 750ms is enough, maybe not
+    // we might do a ds.depower() here, but the reset will take care of it.
+    
+    present = ds.reset();
+    ds.select(addr);    
+    ds.write(0xBE);         // Read Scratchpad
+
+    Serial.print("  Data = ");
+    Serial.print(present, HEX);
+    Serial.print(" ");
+    for ( i = 0; i < 9; i++) {           // we need 9 bytes
+        data[i] = ds.read();
+        Serial.print(data[i], HEX);
+        Serial.print(" ");
+    }
+    Serial.print(" CRC=");
+    Serial.print(OneWire::crc8(data, 8), HEX);
+    Serial.println();
+
+    // Convert the data to actual temperature
+    // because the result is a 16 bit signed integer, it should
+    // be stored to an "int16_t" type, which is always 16 bits
+    // even when compiled on a 32 bit processor.
+    int16_t raw = (data[1] << 8) | data[0];
+    if (type_s) {
+        raw = raw << 3; // 9 bit resolution default
+        if (data[7] == 0x10) {
+            // "count remain" gives full 12 bit resolution
+            raw = (raw & 0xFFF0) + 12 - data[6];
         }
-    }while(seconder > 0);
+    } 
+    else {
+        byte cfg = (data[4] & 0x60);
+        // at lower res, the low bits are undefined, so let's zero them
+        if (cfg == 0x00) {
+            raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+        }
+        else if (cfg == 0x20) {
+             raw = raw & ~3; // 10 bit res, 187.5 ms
+        }
+        else if (cfg == 0x40) {
+             raw = raw & ~1; // 11 bit res, 375 ms
+        } //// default is 12 bit resolution, 750 ms conversion time
+    }
+    float celsius = raw / 16.0;
+    // fahrenheit = celsius * 1.8 + 32.0;
+    Serial.println((String)"Temperature = " + celsius + "C");
+    return celsius; 
 }
--- Sensors.h
+++ Sensors.h
@@ -1,16 +1,62 @@
 #ifndef Sensors_h
 #define Sensors_h
 
-void getSensorReads(int analogPin1, int readChannels[]);
-void getSensorReads(int analogPin1, int analogPin2, int readChannels[]);
-void printSensorReads(int readChannels[], int cycles[]);
-void printSensorReads(int readChannels[]);
-void collectFlags(int readChannels[], bool sensorFlags[]);
-void clearFlags(bool sensorFlags[]);
-bool isDry(int& read, const int moistureThresholdPercentage);
+#include "Config.h"
+#include "OneWire.h"
 
-// Obligatory first command after Arduino start and after every pump activation.
-void calibrateSensors();
+class Sensor {
+    public:
+        bool checkFlag() {
+            return _flag;
+        }
+        bool clearFlag() {
+            _flag = 0;
+        }
+    protected:
+        int _ID;
+        int _minValue;
+        int _maxValue;
+        bool _flag;
+};
 
+class MoistureSensor : Sensor {
+    public:
+        inline MoistureSensor(int ID, int thresholdPercentage, int muxChannel, int analogPin=A0) {
+            _muxChannel = muxChannel;
+            _ID = ID;
+            _analogPin = analogPin;
+            _thresholdPercentage = thresholdPercentage;
+            _minValue = WET_THRESHOLD;
+            _maxValue = DRY_THRESHOLD;
+            _flag = 0;
+            _value = 0;
+        }
+        bool isDry();
+
+    private:
+        int readValue();
+        int mapValue();
+        int _thresholdPercentage;
+
+        int _analogPin;
+        int _muxChannel;
+        int _value;
+};
+
+class TSensorsBus : Sensor { // temperature sensors TODO
+    public:
+        inline TSensorsBus(int digitalPin) { 
+            _digitalPin = TEMP_SENSOR;
+            _flag = 0; }
+        float callSensors();
+
+    private:
+        int _digitalPin;
+        byte i;
+        byte present = 0;
+        byte type_s;
+        byte data[12];
+        byte addr[8];
+};  
 
 #endif  
