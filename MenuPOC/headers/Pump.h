a9f32e05dc27d4f03ba68d3798df9cd27037c646
martinezpl <mszlesz@gmail.com>
2020-08-26 19:55:30 +0200

    pump constructors

--- MenuPOC/headers/Pump.h
+++ MenuPOC/headers/Pump.h
@@ -8,6 +8,11 @@ using namespace std;
 //todo add min, max, unit, stepValue
 class Pump {
     public:
+        inline Pump() {}
+        inline Pump(string n, int v) {
+            name = n;
+            value = v;
+        }
         int value;
     
     private:
